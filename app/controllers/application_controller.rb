aad6f7a8a6d44b4ce8124180c1e4b9faa583253f
EC2 Default User <ec2-user@ip-172-31-1-131.ap-northeast-1.compute.internal>
2025-02-10 19:45:44 +0000

    [Finish] ログインに使う情報 認証キー をemail -> nameに

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::Base
+
+  # 初期状態の devise は、サインアップ、サインイン時に「email」と「パスワード」
+  # しか受け取ることを許可されていません。deviseのコントローラは直接修正
+  # できないため、全てのコントローラに対する処理を行える権限を持つ、
+  # ApplicationControllerに記述する必要があります。
+  # 
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
+  end
+  # ここがnameのままだと、sign_up時は、 入力したemailが抹消?され、
+  # Email can't be blank が出る
+
+
 end
