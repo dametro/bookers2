be31bae7844a1429dbb5b89d40add1ec3405b529
EC2 Default User <ec2-user@ip-172-31-1-131.ap-northeast-1.compute.internal>
2025-02-10 21:18:43 +0000

    [Add] books, users controller作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  # get 'users/index'
+  # get 'users/show'
+  # get 'users/edit'
+  resources :users, only: [:index, :create, :show, :edit, :update, :destroy]
+  # get 'books/index'
+  # get 'books/show'
+  # get 'books/edit'
+  resources :books, only: [:index, :create, :show, :edit, :update, :destroy]
   root to: 'homes#top'
   get '/about' => 'homes#about'
   devise_for :users
