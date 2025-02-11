f65aa49ab39a2e7507a30e0fbd908ef1e3bd019e
EC2 Default User <ec2-user@ip-172-31-1-131.ap-northeast-1.compute.internal>
2025-02-11 04:35:58 +0000

    [Finish] book show 関連 ... view, contr 完成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,6 +5,10 @@ class BooksController < ApplicationController
   end
 
   def show
+    #投稿フォーム:render new_book用
+    @new_book = Book.new
+    #詳細表示用
+    @book = Book.find(params[:id])  
   end
 
   def edit
