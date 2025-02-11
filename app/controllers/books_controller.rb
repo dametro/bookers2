52fafe656947a4c314b6dfa9c1ccb77d4a3b03f6
EC2 Default User <ec2-user@ip-172-31-1-131.ap-northeast-1.compute.internal>
2025-02-11 10:04:06 +0000

    [Update] _new_bookに渡す変数を@new_bookに変更(books)

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,14 +1,18 @@
 class BooksController < ApplicationController
   def index
     @books = Book.all
-    @book = Book.new
+    @new_book = Book.new
+    #左上プロフィール用
+    @user = current_user 
   end
 
   def show
     #投稿フォーム:render new_book用
     @new_book = Book.new
     #詳細表示用
-    @book = Book.find(params[:id])  
+    @book = Book.find(params[:id])
+    #左上プロフィール用  
+    @user = current_user 
   end
 
   def edit
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   def index
+    @user = current_user #右上プロフィール用
   end
 
   def show
