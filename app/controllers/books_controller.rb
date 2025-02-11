7d43a7bc26e910e699579fe138cd21fbbaf3a564
EC2 Default User <ec2-user@ip-172-31-1-131.ap-northeast-1.compute.internal>
2025-02-11 01:43:49 +0000

    [Update]user, book のアソシエーション

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,7 @@
 class BooksController < ApplicationController
   def index
+    @books = Book.all
+    @book = Book.new
   end
 
   def show
@@ -7,4 +9,22 @@ class BooksController < ApplicationController
 
   def edit
   end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to books_path
+  end
+
+
+
+
+  # 投稿データのストロングパラメータ
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -11,8 +11,16 @@ class User < ApplicationRecord
   # :validatable（email のフォーマットなどのバリデーション
    
   has_one_attached :image
-  #デフォルト画像の指定は-- にて
+  #デフォルト画像の指定はget_image にて
+  
+  def get_image
+    unless image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    image
+  end
   
   has_many :books, dependent: :destroy
-  
+
 end
