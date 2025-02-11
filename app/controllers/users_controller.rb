0e5fa86c3be7076356ad99082535058c43daa62d
EC2 Default User <ec2-user@ip-172-31-1-131.ap-northeast-1.compute.internal>
2025-02-11 10:48:22 +0000

    [Update] user show で条件付き一覧を表示成功

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,9 +4,20 @@ class UsersController < ApplicationController
     @users = User.all
     #左上プロフィール用
     @your_user = current_user
+
+    #投稿フォーム:render new_book用
+    @new_book = Book.new
   end
 
   def show
+    #左上、あなたのみではなく、任意の注目ユーザー
+    @user = User.find(params[:id])
+    #その人の投稿したbook
+    @books = Book.where(user_id: @user.id)
+
+    #投稿フォーム:render new_book用
+    @new_book = Book.new
+
   end
 
   def edit
