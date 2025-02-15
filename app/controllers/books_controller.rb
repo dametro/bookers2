b8de4606feb083baa4ad10bf558081945befeb52
EC2 Default User <ec2-user@ip-172-31-1-131.ap-northeast-1.compute.internal>
2025-02-15 07:51:42 +0000

    [Add] ロ book title, body必須化

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -35,7 +35,11 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
+    if @book.save
+      puts "成功！book create."
+    else 
+      puts "失敗...... 。book createできず."
+    end
     redirect_to books_path
   end
 
