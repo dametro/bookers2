872e9fcdf8b5a680b2d813eb8f313977f518df38
EC2 Default User <ec2-user@ip-172-31-1-131.ap-northeast-1.compute.internal>
2025-02-15 05:24:28 +0000

    [Add] before action追加、一部除いて未ログイン時のアクセスを禁止(/users/sign_inへ)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,20 @@
 class ApplicationController < ActionController::Base
 
+
+  before_action :authenticate_user!, except: [:top, :about, :new_user_registration, :new_user_session]
+
+
+# ログイン認証が済んでいない状態で
+# /
+# /sign_up
+# /sign_in
+# /about
+# 以外の画面にアクセスしても、ログイン画面へリダイレクトするように.
+
   # 初期状態の devise は、サインアップ、サインイン時に「email」と「パスワード」
   # しか受け取ることを許可されていません。deviseのコントローラは直接修正
   # できないため、全てのコントローラに対する処理を行える権限を持つ、
   # ApplicationControllerに記述する必要があります。
-  # 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
