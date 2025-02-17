aef5d114edbf189c8f5f414159bae748f447f6d9
EC2 Default User <ec2-user@ip-172-31-1-131.ap-northeast-1.compute.internal>
2025-02-17 14:03:39 +0000

    [Add] いいね機能をbooks index に作成

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,9 +2,16 @@ class FavoritesController < ApplicationController
 
   def create
     book = Book.find(params[:book_id])
-    favorite = current_user.favorites.new(book_id: book.id)
-    favorite.save
-    puts "いいね成功しました"
+    # favorite = current_user.favorites.new(book_id: book.id)
+    favorite = Favorite.new(book_id: book.id)
+    favorite.user_id = current_user.id 
+    
+    puts "新しいfavoriteのデータ... #{favorite}, id:#{favorite.id}, user: #{favorite.user_id}, book: #{favorite.book_id} です  "
+    if favorite.save
+      puts "いいね成功しました"
+    else
+      puts "失敗‼!!! いいね 失敗しました......"
+    end
     # 直前のページにリダイレクト
     # books#index books#show user#show のいずれかを想定
     redirect_to request.referer
@@ -13,8 +20,12 @@ class FavoritesController < ApplicationController
   def destroy
     book = Book.find(params[:book_id])
     favorite = current_user.favorites.find_by(book_id: book.id)
-    favorite.destroy
-    puts "失敗‼!!! いいね 失敗しました......"
+    puts "消したいfavoriteのデータ... #{favorite} です "
+    if favorite.destroy
+      puts "いいねの破壊に成功しました"
+    else
+      puts "失敗‼!!! いいね 失敗しました......"
+    end
     # 直前のページにリダイレクト
     # books#index books#show user#show のいずれかを想定
     redirect_to request.referer
